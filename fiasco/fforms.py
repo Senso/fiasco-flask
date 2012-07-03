79762adff6981247931dfe52ed1d748883ac67f7
Manu <sensoz@gmail.com>
2012-07-03 11:03:54 +0700

    PEP8

--- fiasco/fforms.py
+++ fiasco/fforms.py
@@ -1,14 +1,17 @@
 from wtforms import Form, TextAreaField, TextField, SelectField, validators
 
+
 class NewPlayset(Form):
-	name = TextField('Playset Name', [validators.Length(min=4, max=100), validators.Required()])
-	description = TextAreaField('Description', [validators.Length(min=4), validators.Required()])
+    name = TextField('Playset Name', [validators.Length(min=4, max=100), validators.Required()])
+    description = TextAreaField('Description', [validators.Length(min=4), validators.Required()])
+
 
 class NewGame(Form):
-	name = TextField('Game Name', [validators.Length(min=4, max=50), validators.Required()])
-	playset = SelectField('Choose a Playset', coerce=int)
+    name = TextField('Game Name', [validators.Length(min=4, max=50), validators.Required()])
+    playset = SelectField('Choose a Playset', coerce=int)
+
 
 class EditPlayset(Form):
-	name = TextField('Playset Name', [validators.Length(min=4, max=100), validators.Required()])
-	description = TextAreaField('Description', [validators.Length(min=4), validators.Required()])
-
+    # Most of the fields are generated in views.py
+    name = TextField('Playset Name', [validators.Length(min=4, max=100), validators.Required()])
+    description = TextAreaField('Description', [validators.Length(min=4), validators.Required()])
